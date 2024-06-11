68722a979dee65933a8c22dc77ea28c1fddc6444
Will Taylor <will.taylor@mail.utoronto.ca>
2024-06-10 22:11:59 -0400

    Added subdirectories.

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
-from FCOFFS.Utilities import *
-from FCOFFS.componentClass import *
-from FCOFFS.Node import *
-from FCOFFS.PressureSystem import PressureSystem
+from FCOFFS.utilities.Utilities import *
+from FCOFFS.components.componentClass import *
+from FCOFFS.nodes.Node import *
+from FCOFFS.pressureSystem.PressureSystem import PressureSystem
 
 PS = PressureSystem(ref_p=(15, UnitPressure.psi))
 
